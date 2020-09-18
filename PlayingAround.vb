f470555d8db5adba3c5a6a5721255cf4f6c073b4
Justine Hoffa <justinefarrer@yahoo.com>
2020-09-18 08:56:02 -0600

    Add project files.

---
+++ PlayingAround.vb
@@ -0,0 +1,22 @@
+﻿Module PlayingAround
+
+    Sub Main()
+        LoadArray()
+
+        Console.Read()
+    End Sub
+
+    Sub LoadArray()
+        Dim count As Integer
+        Dim values(4, 19) As Integer
+        For row = 0 To 19
+            For column = 0 To 4
+                Console.Write($"({row},{column})")
+                count += 1
+                values(column, row) = count
+            Next
+            Console.WriteLine()
+        Next
+    End Sub
+
+End Module
